9768ee334d6ee3fac5a086f41605dcc3aabe1031
Alande Amorim <alande.amorim@gmail.com>
2020-04-12 09:47:02 -0300

    eslint and prettier setup

---
+++ App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+export default function App() {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <Text>GitHub List</Text>
+    </>
+  );
+}
